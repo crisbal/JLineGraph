4d08ed073d586c55595e2d97f393ce009c5babc4
cris9696 <bld.cris.96@gmail.com>
2014-05-20 20:12:48 +0200

    Fixed a few things and added setLineColor and setLineWidth method

--- src/example/Example.java
+++ src/example/Example.java
@@ -3,24 +3,27 @@ package example;
 import jPlot.JPlotXY;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by cris9696 on 5/20/14.
  */
 public class Example extends JFrame {
 
-    public Example()
-    {
-        int[] x = {0, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71};
-        int[] y = {177, 348, 246, 266, 116, 158, 299, 313, 182, 60, 24, 303, 386, 228, 513, 360, 448, 544, 314, 384, 488, 99, 34, 534, 463, 228, 169, 19, 423, 166, 167, 78, 435, 232, 1, 322, 9, 427, 473, 452, 283, 582, 473, 83, 36, 560, 67, 418, 78, 193, 201, 402, 166, 596, 235, 452, 125, 593, 325, 262, 418, 487, 243, 55, 351, 248, 421, 58, 224, 18, 442, 104, 297, 317, 584, 389, 50, 406, 318, 405, 143, 108, 363, 170, 528, 457, 312, 490, 239, 254, 29, 141, 34, 594, 434, 360, 319, 345, 571, 484};
-        getContentPane().add(new JPlotXY(x,y,"Example",1280,720));
+    public Example() {
+        long[] x = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        long[] y = {55, 348, 246, 266, 116, 158, 299, 313, 90};
+
+        JPlotXY chart = new JPlotXY(x, y, "Example", 800, 600);
+        chart.setLineColor(Color.green);
+        chart.setLineWidth(2);
+        getContentPane().add(chart);
         pack();
         setTitle("Example");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         new Example().setVisible(true);
 
 
--- src/jPlot/JPlotXY.java
+++ src/jPlot/JPlotXY.java
@@ -14,14 +14,16 @@ public class JPlotXY extends JComponent {
     private static final int PANEL_MARGIN = 30;
     private final int nOfItems;
     private int h, w;
-    private int[] xValues;
-    private int[] yValues;
+    private long[] xValues;
+    private long[] yValues;
     private String plotName;
     private int plotW, plotH;
-    private int rangeX, rangeY;
+    private long rangeX;
+    private long rangeY;
+    private Color lineColor;
+    private int lineWidth;
 
-
-    public JPlotXY(int[] xValues, int[] yValues, String plotName, int w, int h) {
+    public JPlotXY(long[] xValues, long[] yValues, String plotName, int w, int h) {
         super();
         this.xValues = xValues;
         this.yValues = yValues;
@@ -37,6 +39,9 @@ public class JPlotXY extends JComponent {
 
         plotW = w - 2 * PANEL_MARGIN;
         plotH = h - 2 * PANEL_MARGIN;
+
+        lineColor = Color.red;
+        lineWidth = 3;
     }
 
     public Dimension getPreferredSize() {
@@ -47,40 +52,50 @@ public class JPlotXY extends JComponent {
         super.paintComponent(g1);
         Graphics2D g = (Graphics2D) g1;
 
-        g.setColor(Color.black);
-        g.drawLine(PANEL_MARGIN, PANEL_MARGIN, PANEL_MARGIN, h - PANEL_MARGIN * 2);
-        g.drawLine(PANEL_MARGIN, h - PANEL_MARGIN * 2, w - PANEL_MARGIN, h - PANEL_MARGIN * 2);
-        g.drawString(plotName, PANEL_MARGIN, h - PANEL_MARGIN);
+        g.setColor(Color.white);
+        g.fillRect(0,0,w,h);    //background
 
         for (int i = 1; i < nOfItems; i++) {
-            g.setColor(Color.red);
-            g.setStroke(new BasicStroke(3));
+            g.setColor(lineColor);
+            g.setStroke(new BasicStroke(lineWidth));
 
-            int prevX = xValues[i - 1];
-            int currX = xValues[i];
+            long prevX = xValues[i - 1];
+            long currX = xValues[i];
 
-            int prevXatZero = prevX - getMin(xValues);
-            int currXatZero = currX - getMin(xValues);
+            long prevXatZero = prevX - getMin(xValues);
+            long currXatZero = currX - getMin(xValues);
 
-            int drawPrevX = (prevXatZero * plotW) / rangeX;   //it was THAT simple
-            int drawCurrX = (currXatZero * plotW) / rangeX;
+            long drawPrevX = (prevXatZero * plotW) / rangeX;   //it was THAT simple
+            long drawCurrX = (currXatZero * plotW) / rangeX;
 
-            g.drawLine(drawPrevX + PANEL_MARGIN,
-                    h - yValues[i - 1] - PANEL_MARGIN * 2,
-                    drawCurrX + PANEL_MARGIN,
-                    h - yValues[i] - PANEL_MARGIN * 2);
+            long prevY = yValues[i - 1];
+            long currY = yValues[i];
+
+            long prevYatZero = prevY - getMin(yValues);
+            long currYatZero = currY - getMin(yValues);
+
+            int drawPrevY = (int) ((prevYatZero * plotH) / rangeY);   //it was THAT simple
+            int drawCurrY = (int) ((currYatZero * plotH) / rangeY);
+
+
+            g.drawLine((int)(drawPrevX + PANEL_MARGIN),
+                    h - drawPrevY - PANEL_MARGIN * 2,
+                    (int) (drawCurrX + PANEL_MARGIN),
+                    h - drawCurrY - PANEL_MARGIN * 2);
 
             g.setColor(Color.black);
             g.setStroke(new BasicStroke(1));
-
-            g.drawString(prevX + "", drawPrevX + PANEL_MARGIN, h - PANEL_MARGIN * 2 + 15);
-            g.drawString(currX + "", drawCurrX + PANEL_MARGIN, h - PANEL_MARGIN * 2 + 15);
-
         }
+
+        g.setColor(Color.black);
+        g.drawLine(PANEL_MARGIN, PANEL_MARGIN, PANEL_MARGIN, h - PANEL_MARGIN * 2); //x axis
+        g.drawLine(PANEL_MARGIN, h - PANEL_MARGIN * 2, w - PANEL_MARGIN, h - PANEL_MARGIN * 2); //y axis
+
+        g.drawString(plotName, (w-PANEL_MARGIN)/2, h - PANEL_MARGIN);   //graph name
     }
 
-    public int getMin(int[] array) {
-        int min = 0;
+    public long getMin(long[] array) {
+        long min = 0;
         for (int i = 0; i < array.length; i++) {
             if (i == 0 || array[i] < min)
                 min = array[i];
@@ -88,12 +103,22 @@ public class JPlotXY extends JComponent {
         return min;
     }
 
-    public int getMax(int[] array) {
-        int max = 0;
+    public long getMax(long[] array) {
+        long max = 0;
         for (int i = 0; i < array.length; i++) {
             if (i == 0 || array[i] > max)
                 max = array[i];
         }
         return max;
+    }
+
+    public void setLineColor(Color c)
+    {
+        lineColor = c;
+    }
+
+    public void setLineWidth(int w)
+    {
+        lineWidth = w;
     }
 }
